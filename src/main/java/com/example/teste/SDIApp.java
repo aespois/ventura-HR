1b763cbf9e5ae4c1feb3a998ba11f81bd97d48ff
Ana Espois <ana.espois@cortex-intelligence.com>
2021-03-02 19:18:51 -0300

    JPA config

---
+++ src/main/java/com/example/teste/SDIApp.java
@@ -0,0 +1,13 @@
+package com.example.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDIApp {
+
+	public static void main(String[] args) {
+            SpringApplication.run(SDIApp.class, args);
+	}
+
+}

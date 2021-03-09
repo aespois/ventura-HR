3b64a5adec1867d84361ca482d44cf67bd8e2758
Ana Espois <ana.espois@cortex-intelligence.com>
2021-03-09 04:18:52 -0300

    Microsserviço de Usuário + Login

--- src/main/java/br/edu/infnet/infra/usuarios/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.infnet.infra.usuarios;
-
-import br.edu.infnet.domain.usuarios.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
-    Usuario findByEmail(String email);
-}

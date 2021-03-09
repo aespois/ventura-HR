1b9335a82b2c8d90c0a5a6117d1656a69ae7f749
Ana Espois <ana.espois@cortex-intelligence.com>
2021-03-09 02:16:28 -0300

    Microsserviço de Usuário

--- src/main/java/br/edu/infnet/VenturaHrApplication.java
+++ src/main/java/br/edu/infnet/VenturaHrApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VenturaHrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VenturaHrApplication.class, args);
-	}
+    public static void main(String[] args) {
+            SpringApplication.run(VenturaHrApplication.class, args);
+    }
 
 }
--- src/test/java/br/edu/infnet/VenturaHrApplicationTests.java
+++ src/test/java/br/edu/infnet/VenturaHrApplicationTests.java
@@ -2,8 +2,10 @@ package br.edu.infnet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration
 class VenturaHrApplicationTests {
 
 	@Test
